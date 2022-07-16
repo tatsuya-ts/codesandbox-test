275c4120cc6355475017bf34b1157caf44982f85
ultra <47292936+tatsuya-ts@users.noreply.github.com>
2022-07-17 01:59:01 +0900

    タスク追加機能を追加

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // buttonタグ（完了）生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+
+  // buttonタグ（削除）生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
